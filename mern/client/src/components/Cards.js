8c5ce82c632fb0b758212f3b6016b784646055d3
Ploy Unchit <ployunchit0@gmail.com>
2022-11-01 11:24:57 -0400

    added pictures

--- mern/client/src/components/Cards.js
+++ mern/client/src/components/Cards.js
@@ -11,13 +11,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
+              src='/img-1.jpeg'
               text='9:00 PM 10/30/22'
               label='Tennis'
               path='/signup'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='/img-2.jpeg'
               text='3:00 PM 10/31/22'
               label='Skateboarding'
               path='/signup'
@@ -25,19 +25,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='/img-3.jpeg'
               text='5:00 PM 11/3/22'
               label='Volleyball'
               path='/signup'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='/img-4.jpeg'
               text='2:00 PM 11/4/22'
               label='Canoeing'
               path='/signup'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='/img-5.jpeg'
               text='1:00 PM 11/5/22'
               label='Rock Climbing'
               path='/signup'
